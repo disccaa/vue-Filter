ca477f84d5667ff7e9fcb84eeac50e936734aef7
Your Name <isabil177@mail.ru>
2022-04-24 21:29:18 +0600

    filter, Categories, price filter

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/products">Products</RouterLink>|
+    <RouterLink to="/products/:id">Product</RouterLink>|
+  </div>
+    <RouterView />
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style scoped>
+</style>
